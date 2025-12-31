c3c6663f84a0a1da3a635a41804dbbd4f373e95a
Colin <colinwenk@web.de>
2025-12-31 18:08:06 +0100

    Added Times rolled counter

--- Dice-Rolling-Game.py
+++ Dice-Rolling-Game.py
@@ -1,5 +1,5 @@
 import random
-
+times_rolled = 0
 while True:
     choice = input("Roll the dice? (y/n): ").strip().lower()
     if choice == "y":
@@ -11,10 +11,12 @@ while True:
         except ValueError:
             print("Please enter a valid number.")
             continue
+        times_rolled += roll_amount
         for i in range(roll_amount):
             num1 = random.randint(1, 6)
             num2 = random.randint(1, 6)
             print(f'({num1}, {num2})')
+        print("Times rolled:", times_rolled)
     elif choice == "n":
         print("Thanks for playing!")
         break
